418c506375e03e879f9c904f999ee58ef0f17346
grigan-Uragan <prischepny@mail.ru>
2020-07-15 17:07:42 +0300

    4.1. Операторы ветвлений [#326340]

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+
+    public static void possibleDivideByNumber(int number) {
+        if (number == 0) {
+            System.out.println("Could not divided by zero");
+        } else if (number < 0) {
+            System.out.println("This is negative numbers");
+        } else {
+            System.out.println("ok");
+        }
+    }
+
+    public static void main(String[] args) {
+        possibleDivideByNumber(0);
+        possibleDivideByNumber(-10);
+        possibleDivideByNumber(10);
+    }
+}
