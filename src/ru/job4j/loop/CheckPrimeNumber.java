6ee09382a193504b19a3fd132c067586c39dbcd7
grigan-Uragan <prischepny@mail.ru>
2020-07-18 15:13:39 +0300

    5.5. Простое число [#326430]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean isPrime = false;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            } else {
+                isPrime = true;
+            }
+        }
+        return isPrime;
+    }
+}
