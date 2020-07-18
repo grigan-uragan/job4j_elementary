782c98dfd2b21ddedd0e7bdd80006fa20361e28d
grigan-Uragan <prischepny@mail.ru>
2020-07-18 17:48:34 +0300

    6.0.2. Заполнение массива. [#326358]

--- src/ru/job4j/array/Definition.java
+++ src/ru/job4j/array/Definition.java
@@ -5,5 +5,13 @@ public class Definition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Mark Twen";
+        names[1] = "Edgar Po";
+        names[2] = "John Doe";
+        names[3] = "Jack London";
+        for (String str : names) {
+            System.out.println(str);
+        }
     }
 }
