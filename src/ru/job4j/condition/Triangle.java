07e2f1dc9e47b9dd0b99a535623297cbe7a9b0f7
grigan-Uragan <prischepny@mail.ru>
2020-07-22 00:08:27 +0300

    2.2. Рефакторинг - Площадь треугольника. [#326511]

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -1,7 +1,33 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point a, Point b, Point c) {
+        first = a;
+        second = b;
+        third = c;
+    }
+
+    public double period(double ab, double bc, double ac) {
+        return (ab + bc + ac) / 2;
+    }
+
+    public boolean exist(double ab, double bc, double ac) {
         return ((ab + ac) > bc && (ab + bc) > ac && (bc + ac) > ab);
+    }
+
+    public double area() {
+        double result = -1;
+        double ab = first.distance(second);
+        double bc = second.distance(third);
+        double ac = first.distance(third);
+        if (exist(ab, bc, ac)) {
+            double p = period(ab, bc, ac);
+            result = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return result;
     }
 }
--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -2,23 +2,19 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class TriangleTest {
 
     @Test
-    public void whenOneSideZeroThenFalse() {
-        assertThat(Triangle.exist(2.0, 0, 2.0), is(false));
-    }
-
-    @Test
-    public void whenAllSide2ThenTrue() {
-        assertThat(Triangle.exist(2, 2, 2), is(true));
-    }
-
-    @Test
-    public void whenOneSideVeryBigThenFalse() {
-        assertThat(Triangle.exist(100, 2, 3), is(false));
+    public void period() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
     }
 }
