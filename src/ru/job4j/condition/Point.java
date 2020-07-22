6aff5894fd859cdebcd7760c722961bf932a83e3
grigan-Uragan <prischepny@mail.ru>
2020-07-22 22:00:13 +0300

    4. Расстояние между точками в трехмерном пространстве. [#326512]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -3,22 +3,35 @@ package ru.job4j.condition;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point point) {
         int x = point.x - this.x;
         int y = point.y - this.y;
-        double result = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-        return result;
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+
+    public double distance3d(Point point) {
+        int x = point.x - this.x;
+        int y = point.y - this.y;
+        int z = point.z - this.z;
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
     }
 
     public static void main(String[] args) {
-        Point first = new Point(0, 0);
-        Point second = new Point(10, 10);
-        System.out.println(first.distance(second));
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(10, 10, 10);
+        System.out.println(first.distance3d(second));
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -22,4 +22,22 @@ public class PointTest {
         double expected = 14.14;
         Assert.assertEquals(result, expected, 0.01);
     }
+
+    @Test
+    public void when3DPointEqualsThenResultZero() {
+        Point first = new Point(10, 10, 10);
+        Point second = new Point(10, 10, 10);
+        double result = first.distance3d(second);
+        double expected = 0;
+        Assert.assertEquals(result, expected, 0.01);
+    }
+
+    @Test
+    public void when3DPointDifferenceThenResultMoreThanZero() {
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(10, 10, 10);
+        double result = first.distance3d(second);
+        double expected = 17.32;
+        Assert.assertEquals(result, expected, 0.01);
+    }
 }
