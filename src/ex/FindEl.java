4e3bd4d047abc6a8e00a20a8a78e0eb6db05c699
grigan-Uragan <prischepny@mail.ru>
2020-07-28 00:57:49 +0300

    0.4. Пользовательские исключения. [#326501]

---
+++ src/ex/FindEl.java
@@ -0,0 +1,29 @@
+package ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] values = {
+                "Hello", "World", "London", "The Capital"
+        };
+        int index = -1;
+        try {
+            index = indexOf(values, "Moscow");
+        } catch (ElementNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(index);
+    }
+}
