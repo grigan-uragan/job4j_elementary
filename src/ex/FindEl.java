ddbc25dd89f5b0a3443bf041f45b59357f745877
grigan-Uragan <prischepny@mail.ru>
2020-07-28 11:29:44 +0300

    0.4. Пользовательские исключения. [#326501]

--- src/ex/FindEl.java
+++ src/ex/FindEl.java
@@ -6,6 +6,7 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
                 result = i;
+                break;
             }
         }
         if (result == -1) {
